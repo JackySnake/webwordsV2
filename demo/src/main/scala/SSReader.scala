d768458dac1d23d86e79c6e61db6c9ccded583f3
tien.nguyenv <tien.nguyenv@smartdev.vn>
2015-08-31 11:17:06 +0700

    url -> text -> extraction

--- demo/src/main/scala/SSReader.scala
+++ demo/src/main/scala/SSReader.scala
@@ -1,11 +1,21 @@
 import java.net.URL
 
 import com.kohlschutter.boilerpipe.extractors.ArticleExtractor
+import uk.ac.shef.dcs.jate.model.{Term, InMemoryDocument}
+import uk.ac.shef.dcs.jate.processing.KeywordExtraction
 
 /**
  * Created by joseph on 31/08/2015.
  */
 object SSReader extends App {
-  val url: URL = new URL("http://www.npr.org/sections/health-shots/2013/10/18/236211811/brains-sweep-themselves-clean-of-toxins-during-sleep")
-  System.out.println(ArticleExtractor.INSTANCE.getText(url))
+    val url: URL = new URL("http://www.npr.org/sections/health-shots/2013/10/18/236211811/brains-sweep-themselves-clean-of-toxins-during-sleep")
+//  System.out.println(ArticleExtractor.INSTANCE.getText(url))
+    val article = ArticleExtractor.INSTANCE.getText(url)
+
+    val document = new InMemoryDocument(article);
+    val extraction = new KeywordExtraction(document);
+    val terms: Array[Term] = extraction.extractKeywords();
+    terms.foreach(x => println("sdlfsdfj %s %s", x.getConcept, x.getConfidence))
+
+
 }
