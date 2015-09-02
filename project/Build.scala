3a9c29da8beb870b5cb8456651e0c73b3118aa7f
josephdevz <joseph.devz@gmail.com>
2015-09-03 00:56:30 +0700

    sample

--- project/Build.scala
+++ project/Build.scala
@@ -29,6 +29,7 @@ object BuildSettings {
           , thenewmotion
 //          , ossrh
 //          , apache
+          , twitter
         ))
 
 //  resolvers += Resolver.sonatypeRepo("snapshots"),
@@ -40,84 +41,71 @@ object BuildSettings {
 }
 
 object Resolvers {
-    val sonatypeRepo = "Sonatype Release" at "http://oss.sonatype.org/content/repositories/releases"
-  val snapshots = "snapshots"           at "http://oss.sonatype.org/content/repositories/snapshots"
-  val jbossRepo = "JBoss" at "http://repository.jboss.org/nexus/content/groups/public/"
+    val sonatypeRepo  = "Sonatype Release" at "http://oss.sonatype.org/content/repositories/releases"
+    val snapshots     = "snapshots"           at "http://oss.sonatype.org/content/repositories/snapshots"
+    val jbossRepo     = "JBoss" at "http://repository.jboss.org/nexus/content/groups/public/"
 
-    val akkaRepo = "Akka" at "http://repo.akka.io/repository/"
-  val releases = "releases"            at "http://oss.sonatype.org/content/repositories/releases"
-  val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
-  val hseeberger = "hseeberger at bintray" at "http://dl.bintray.com/hseeberger/maven"
+    val akkaRepo      = "Akka" at "http://repo.akka.io/repository/"
+    val releases      = "releases"            at "http://oss.sonatype.org/content/repositories/releases"
+    val typesafe      = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+    val hseeberger    = "hseeberger at bintray" at "http://dl.bintray.com/hseeberger/maven"
 
-  val thenewmotion = "The New Motion Public Repo" at "http://nexus.thenewmotion.com/content/groups/public/"
+    val thenewmotion  = "The New Motion Public Repo" at "http://nexus.thenewmotion.com/content/groups/public/"
 
 //  val ossrh = "NLP" at "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
 
 //  val apache = "apache opennlp releases" at "https://repository.apache.org/content/repositories/releases/"
+
+    val twitter       = "Twitter" at "http://maven.twttr.com"
 }
 
 object Dependencies {
-    val lib = "org.scala-lang" % "scala-library" % "2.10.4"
+    val lib             = "org.scala-lang" % "scala-library" % "2.10.4"
 
 //    val scalatest = "org.scalatest" % "scalatest_2.10" % "2.2.5"
 
-    val slf4jSimple = "org.slf4j" % "slf4j-simple" % "1.6.2"
+    val slf4jSimple     = "org.slf4j" % "slf4j-simple" % "1.6.2"
     val slf4jSimpleTest = slf4jSimple % "test"
 
-    val jettyVersion = "7.4.0.v20110414"
-    val jettyServer = "org.eclipse.jetty" % "jetty-server" % jettyVersion
-    val jettyServlet = "org.eclipse.jetty" % "jetty-servlet" % jettyVersion
+    val jettyVersion    = "7.4.0.v20110414"
+    val jettyServer     = "org.eclipse.jetty" % "jetty-server" % jettyVersion
+    val jettyServlet    = "org.eclipse.jetty" % "jetty-servlet" % jettyVersion
     val jettyServerTest = jettyServer % "test"
 
-    val asyncHttp = "com.ning" % "async-http-client" % "1.6.5"
-
-    val jsoup = "org.jsoup" % "jsoup" % "1.6.1"
-
-    val casbahCore = "org.mongodb" % "casbah-core_2.10" % "2.8.2"
-
-    val actor     = "com.typesafe.akka" %% "akka-actor" % "2.3.7"
-    val rabbitmq  = "com.thenewmotion.akka" %% "akka-rabbitmq" % "1.2.4"
+    val asyncHttp       = "com.ning" % "async-http-client" % "1.6.5"
+    val jsoup           = "org.jsoup" % "jsoup" % "1.6.1"
+    val casbahCore      = "org.mongodb" % "casbah-core_2.10" % "2.8.2"
+    val actor           = "com.typesafe.akka" %% "akka-actor" % "2.3.7"
+    val rabbitmq        = "com.thenewmotion.akka" %% "akka-rabbitmq" % "1.2.4"
 //    val stream    = "com.typesafe.akka" %%  "akka-stream-experimental" % "1.0"
 //      val httpCore  = "com.typesafe.akka" % "akka-http-core-experimental_2.10" % "1.0"
 //    val http      = "com.typesafe.akka" %% "akka-http-experimental" % "1.0"
 
-    val sse = "de.heikoseeberger" %% "akka-sse" % "1.0.0"
-
+    val sse       = "de.heikoseeberger" %% "akka-sse" % "1.0.0"
     val logging   = "com.typesafe.scala-logging" %%  "scala-logging-slf4j"      % "2.1.2"
     val config    = "com.typesafe" % "config" % "1.2.1"
-
     val rabbit    = "io.scalac" %% "reactive-rabbit" % "1.0.1"
     val logbak    = "ch.qos.logback"  %   "logback-core"             % "1.1.2"
     val logcore   = "ch.qos.logback" %   "logback-classic"          % "1.1.2"
-
     val scalatest = "org.scalatest"              %%  "scalatest"                % "2.2.1" % "test"
-
-    val gson = "com.google.code.gson" % "gson" % "2.3.1"
+    val gson      = "com.google.code.gson" % "gson" % "2.3.1"
     val commonsio = "commons-io" % "commons-io" % "2.4"
+    val opennlp   = "org.apache.opennlp" % "opennlp-tools" % "1.5.3"
+    val jwnl      = "net.sf.jwordnet" % "jwnl" % "1.3.3"
+    val xerces    = "xerces" % "xercesImpl" % "2.9.1"
+    val nekohtml  = "net.sourceforge.nekohtml" % "nekohtml" % "1.9.13"
+    val fihttp    = "com.twitter" %% "finagle-http" % "6.28.0"
+    val fihttpx   = "com.twitter" %% "finagle-httpx" % "6.28.0"
+    val mapper    = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.9.13"
+    val finagle   = "com.twitter" %% "finagle-core" % "6.28.0"
+    val finchCore = "com.github.finagle" %% "finch-core" % "0.8.0"
+    val argonaut  = "com.github.finagle" %% "finch-argonaut" % "0.8.0"
+    val jackson   = "com.github.finagle" %% "finch-jackson" % "0.8.0"
+    val json4s    = "com.github.finagle" %% "finch-json4s" % "0.8.0"
+    val circe     = "com.github.finagle" %% "finch-circe" % "0.8.0"
+    val finatraHttp   = "com.twitter.finatra" %% "finatra-http" % "2.0.0.M2"
 
-    val opennlp = "org.apache.opennlp" % "opennlp-tools" % "1.5.3"
-    val jwnl = "net.sf.jwordnet" % "jwnl" % "1.3.3"
-
-  val xerces = "xerces" % "xercesImpl" % "2.9.1"
-
-  val nekohtml = "net.sourceforge.nekohtml" % "nekohtml" % "1.9.13"
-
-  val fihttp = "com.twitter" %% "finagle-http" % "6.28.0"
-  val fihttpx = "com.twitter" %% "finagle-httpx" % "6.28.0"
-
-  val mapper = "org.codehaus.jackson" % "jackson-mapper-asl" % "1.9.13"
-
-  val finagle = "com.twitter" %% "finagle-core" % "6.28.0"
-
-  val finchCore = "com.github.finagle" %% "finch-core" % "0.8.0"
-
-  val argonaut = "com.github.finagle" %% "finch-argonaut" % "0.8.0"
-
-  val jackson = "com.github.finagle" %% "finch-jackson" % "0.8.0"
-
-  val json4s = "com.github.finagle" %% "finch-json4s" % "0.8.0"
-
-  val circe = "com.github.finagle" %% "finch-circe" % "0.8.0"
+    val  finatra = "com.twitter" %% "finatra" % "1.5.3"
 }
 
 object WebWordsBuild extends Build {
@@ -236,6 +224,8 @@ object WebWordsBuild extends Build {
         , json4s
         , circe
         , finagle
+        , finatra
+        , finatraHttp
       )))
 
 }
--- restful-service/src/main/scala/io/finch/petstore/endpoint.scala
+++ restful-service/src/main/scala/io/finch/petstore/endpoint.scala
@@ -1,14 +1,13 @@
 package io.finch.petstore
 
 import com.twitter.finagle.Service
-import com.twitter.finagle.httpx.path.{/, Root, ->}
-import com.twitter.finagle.httpx.{Method, Request, Response}
+import com.twitter.finagle.httpx.{Request, Response}
 import com.twitter.util.{Future}
 import io.finch._
 import io.finch.argonaut._
 import io.finch.request._
 import io.finch.response._
-import io.finch.route.Router
+import io.finch.route._
 
 /**
  * Provides the paths and endpoints for all the API's public service methods.
@@ -21,64 +20,27 @@ object endpoint extends ErrorHandling {
    */
   private def users(db: PetstoreDb) =
     addUser(db) :+:
-    addUsersViaList(db) :+:
-    addUsersViaArray(db) :+:
-    getUser(db) :+:
-    deleteUser(db) :+:
-    updateUser(db)
+      addUsersViaList(db) :+:
+      addUsersViaArray(db)
+//      getUser(db) :+:
+//      deleteUser(db)
+//      updateUser(db)
 
   /**
    * Compiles together all the endpoints relating to public service methods.
    * @return A service that contains all provided endpoints of the API.
    */
   def makeService(db: PetstoreDb): Service[Request, Response] = handleExceptions andThen (
-    users(db)
-  ).toService
-
-  def route = {
-    case Method.Get -> Root / "hello" / name =>
-      //curl 'http://localhost:9090/hello/Andrea'
-      Service.mk(req =>
-        Ok(s"Hello ${name}").toFuture
-      )
-    /*case Method.Post -> Root / "user" =>
-      //curl -X POST 'http://localhost:9090/user?name=Andrea&age=65'
-      Service.mk(req => {
-        for {
-          name <- RequiredParam("name")(req)
-          age <- RequiredParam("age")(req).map(_.toInt)
-        } yield {
-          val user = User(name, age)
-          Ok(s"Hello ${user.greet}")
-        }
-      })*/
-    case _ -> path =>
-      Service.mk(req =>
-        BadRequest(s"Service not found for path: ${path.toString.replace(Root.toString, "")}").toFuture
-      )
-  }
-
+      users(db)
+    ).toService
 
   /**
    * The information of the added User is passed in the body.
    * @return A Router that contains a RequestReader of the username of the added User.
    */
-  /*def addUser(db: PetstoreDb): Router[String] =
+  def addUser(db: PetstoreDb): Router[String] =
     post("user" ? body.as[User]) { u: User =>
-      db.addUser(u)
-    }*/
-
-  def addUser(db: PetstoreDb): Router[String] = {
-    /*post("user" ? body.as[User]) { u: User =>
-      db.addUser(u)*/
-
-    case Method.Get -> Root / "hello" / name =>
-      //curl 'http://localhost:9090/hello/Andrea'
-      Service.mk(req =>
-        Ok(s"Hello ${name}").toFuture
-      )
-
-
+      db.addUser(u).asInstanceOf[String]
     }
 
   /**
@@ -103,27 +65,26 @@ object endpoint extends ErrorHandling {
    * The username of the User to be deleted is passed in the path.
    * @return A Router that contains essentially nothing unless an error is thrown.
    */
-  def deleteUser(db: PetstoreDb): Router[Unit] =
+  /*def deleteUser(db: PetstoreDb): Router[Unit] =
     delete("user" / string) { n: String =>
       db.deleteUser(n)
-    }
+    }*/
 
   /**
    * The username of the User to be found is passed in the path.
    * @return A Router that contains the User in question.
    */
-  def getUser(db: PetstoreDb): Router[User] =
-    get("user" / name) { n: String =>
+  /*def getUser(db: PetstoreDb): Router[User] =
+    get("user" / string) { n: String =>
       db.getUser(n)
-    }
+    }*/
 
   /**
    * The username of the User to be updated is passed in the path.
    * @return A Router that contains a RequestReader of the User updated.
    */
-  def updateUser(db: PetstoreDb): Router[User] =
+  /*def updateUser(db: PetstoreDb): Router[User] =
     put("user" / string ? body.as[User]) { (n: String, u: User) =>
       db.updateUser(u)
-    }
+    }*/
 }
-
