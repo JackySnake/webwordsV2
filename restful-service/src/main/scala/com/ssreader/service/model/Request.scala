f3d90de563b4134c06b22a436f454cd54a01ae1e
josephdevz <joseph.devz@gmail.com>
2015-09-04 01:10:02 +0700

    sample

--- restful-service/src/main/scala/com/ssreader/service/model/Request.scala
+++ restful-service/src/main/scala/com/ssreader/service/model/Request.scala
@@ -4,4 +4,6 @@ package com.ssreader.service.model
  * Created by joseph on 9/3/15.
  */
 sealed trait Request
-final case class ArticleLink(username: Option[String] = None, password: Option[String] = None, link: String) extends Request
+final case class ArticleLink(username: Option[String] = None,
+                             password: Option[String] = None,
+                             link: String) extends Request
